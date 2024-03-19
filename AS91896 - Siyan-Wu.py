2ea99da4cd5ffd40295d57ff4c55bb2a521a2947
Siyan-W <nzsiyanwu@gmail.com>
2024-03-20 11:17:56 +1300

    initial push

--- AS91896 - Siyan-Wu.py
+++ AS91896 - Siyan-Wu.py
@@ -74,8 +74,10 @@ def print_task(task_id):
                   f'Priority: {task_details["Priority"]}\n' \
                   f'Status: {task_details["Status"]}'
         easygui.msgbox(message, title='Task Details')
+
 # Get user input for Task ID
 task_id = easygui.enterbox('Enter Task ID (eg., T1, T2, T3, ...):')
+
 # Check if the entered Task ID is valid and display task details
 if task_id in Task_Dictionary:
     print_task(task_id)
